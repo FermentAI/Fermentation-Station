2d56a9f99b9caa8bb19a35f9632ebc4bf5c28a31
DIEGOA363 <DIEGOA363@users.noreply.github.com>
2021-02-24 23:04:06 -0800

    first sketch of time-variable integration

--- rms/scratch.py
+++ rms/scratch.py
@@ -1,7 +1,6 @@
-from model_def import Model 
+from model_def import Model, Simulator
 import os
 import glob
-from pyfoomb import Caretaker
 from pyfoomb import Visualization
 
 path = os.getcwd()[:-4]
@@ -9,8 +8,12 @@ models = glob.glob(os.path.join(path,'models/**'))
 
 a = Model(models[0])
 
-caretaker = Caretaker(
+sim = Simulator(
+    model = a,
+    t= 8,
     bioprocess_model_class=a.model_class, 
     model_parameters=a.get_all_params(), 
     initial_values=a.get_state(),
 )
+
+data = sim.run()
