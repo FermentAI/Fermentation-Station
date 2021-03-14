2eaee2b77361352121263e38dd8176b22b3d9d19
Shweta Bhushan <sb21321@gmail.com>
2021-03-13 21:04:54 -0800

    modified revisions of app4 with some dynamic graph code

--- rms/dash_apps/apps/app5.py
+++ rms/dash_apps/apps/app5.py
@@ -4,8 +4,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State
 from dash_apps.shared_components import *
 from dash_apps.apps.myapp import app
-
-
+import dash
 
 from engine import Model, Simulator
 import os
@@ -13,23 +12,66 @@ import plotly.express as px
 
 
 path = os.getcwd()
-models = [os.path.join(path,'rms','models', o) for o in sorted(os.listdir(os.path.join('rms','models'))) if os.path.isd$print(models,type(models[0]))
+# get all models in the models directory, skip penicilin
+model_names = [o for o in sorted(os.listdir(os.path.join('rms','models'))) if os.path.isdir(os.path.join('rms','models',o))]
+model_names.remove('penicillin_goldrick_2017')
+model_path = lambda model_name: os.path.join(path,'rms','models', model_name) 
 
-sim = Simulator(model = Model(models[0]))
+# make a Dropdown Menu to select a models
+dropdown_models = lambda pick: [dbc.DropdownMenuItem(m, id = m, active = True) if i is pick else dbc.DropdownMenuItem(m, id = m,  active = False) for i,m in enumerate(model_names)]
 
-lol = sim.model.mvars.default
-mvars = [NamedSlider(
-            name= lol.loc[var,'Label'],
-            id="slider-"+var,
-            min=0,
-            max=500,
-            step=100,
-            value = lol.loc[var,'Value'],
-            other = {'units':lol.loc[var,'Units']}
-            )
-     for var in lol.index]
+def sim(model_name):
+    """
+    Generates simulator object based on model name
 
-run_btn = dbc.Button(children = "Run Simulation", outline=True, size = "lg", color="primary", className="mr-1", id="btn$sim_plot = dcc.Graph(id = 'sim_plot')
+    Arguments
+    ---------
+        model_name
+    """ 
+    simuluator = Simulator(model = Model(model_path(model_name)))
+    variables = simuluator.model.mvars.default
+    return simuluator, variables
+
+def mvars(vars_df):
+    """
+    Generates sliders based on the variables in a DataFrame
+
+    Arguments
+    ---------
+        vars_df: Pandas DataFrame containing variables
+    """ 
+    sliders = []
+    for var in vars_df.index:
+        if vars_df.loc[var,'Min'] is not False:
+            minval = vars_df.loc[var,'Min']
+        else:
+            minval = vars_df.loc[var,'Value']*0.1
+
+        if vars_df.loc[var,'Max'] is not False:
+            maxval = vars_df.loc[var,'Max']
+        else:
+            maxval = vars_df.loc[var,'Value']*1.9
+
+        slider = NamedSlider(
+                    name= vars_df.loc[var,'Label'],
+                    id="slider-"+var,
+                    min=minval,
+                    max=maxval,
+                    step=(maxval-minval)/100,
+                    value = vars_df.loc[var,'Value'],
+                    other = {'units':vars_df.loc[var,'Units']}
+                )
+        sliders.append(slider)
+    return sliders
+
+# default to the first model 
+mysim, myvars = sim(model_names[0])
+
+# make a button to run the simulator
+run_btn = dbc.Button(children = "Run Simulation", outline=True, size = "lg", color="primary", className="mr-1", id="btn_run")
+
+# make a plot for output 
+#sim_plot = dcc.Graph(id = 'sim_plot4')
 
 app = dash.Dash(__name__)
 
@@ -58,7 +100,7 @@ def display_graphs(n_click, div_children):
                 figure={}
             ),
             dcc.RadioItems(
-                id={'type':'bar',
+                id={'type':'dynamic-choice',
                     'index':n_clicks
                 },
                 options=[{'label':'Bar Chart', 'value': 'bar'},
@@ -68,44 +110,68 @@ def display_graphs(n_click, div_children):
             ),
             dcc.Dropdown(
                 id={
-                    'type': 'dynamic-dpn-var',
+                    'type': 'dynamic-dpn-var1',
                     'index': n_clicks
                 },
-                options=[{'label': var, 'value': lol.loc[var,'Label']} for var in lol.index],
+                options=[{'label': var, 'value': var} for var in np.sort(mysim.model.mvars.current.loc['Label']).unique()],
                 multi=True,
                 value=["F","V0"],
+                clearable=False
             ),
             dcc.Dropdown(
                 id={
-                    'type': 'dynamic-dpn-outvar',
+                    'type': 'dynamic-dpn-var2',
                     'index': n_clicks
                 },
-                options=[{'label':
-'''
-dropdown_items = [
-    dbc.DropdownMenuItem(models[0]),
-    dbc.DropdownMenuItem(models[1]),
-    dbc.DropdownMenuItem(models[2]),
-]
+                options=[{'label': var, 'value': var} for var in np.sort(mysim.model.mvars.current.loc['Label']).unique()],
+                multi=True,
+                value=["V0","F"],
+                clearable=False
+            )
+        ]
+    )
+    div_children.append(new_child)
+    return div_children
 
+@app.callback(
+    Output({'type': 'dynamic-graph', 'index':MATCH}, 'figure'),
+    [Input(component_id={'type': 'dynamic-dpn-var1', 'index': MATCH}, component_property='value'),
+     Input(component_id={'type': 'dynamic-dpn-var2', 'index': MATCH}, component_property='value'),
+     Input(component_id={'type': 'dynamic-choice', 'index': MATCH}, component_property='value')]
+)
+
+'''def update_graph(var_1, var_2, chart_choice):
+    print(var_1)
+    new_df = mysim.model.mvars.current.loc[mysim.model.mvars.current.loc['Label'].isin(var_1)]
+    
+    if chart_choice == 'bar':
+        new_df = new_df.groupby(['''
+
+
+
+
+
+'''
+# layout all the components to be displayed
 content = html.Div(
     [
         dbc.Row([
             dbc.Col(
                 dbc.DropdownMenu(
                     label = "Select a model",
-                    children = dropdown_items,
+                    children = dropdown_models(0),
                     right=False,
+                    id = 'dd_models'
                 ),
             )
         ]),
-        dbc.Row(dbc.Label("Alo, This is a row", width=2)),
+        dbc.Row(),
         dbc.Row([
-            dbc.Col(id = 'sliders', children = mvars, width = 2),
+            dbc.Col(id = 'sliders', children = mvars(myvars), width = 2),
             dbc.Col(sim_plot, width = 9)
         ]),
         dbc.Row(run_btn),
-        dbc.Row(id = 'mvars')
+        dbc.Row(id = 'mvars4')
     ],
     id="page-content",
     style = CONTENT_STYLE
@@ -114,58 +180,65 @@ content = html.Div(
 layout = html.Div(
     [
         content,
-        html.Div(id='dummy-output')
+        html.Div(id='dummy-output4')
     ],
 )
 
-
+# callback to update the simulator with the selected model
 @app.callback(
-    Output('dummy-output', 'children'),
-    [Input('slider-'+var, 'value') for var in lol.index])
+    [Output("dd_models", "children"),
+    Output("sliders", "children")],
+    [Input(m, "n_clicks") for m in model_names],
+)
+def update_label(*args):
+    ctx = dash.callback_context
+    # this gets the id of the button that triggered the callback
+    button_id = ctx.triggered[0]["prop_id"].split(".")[0]
 
-def update_mvars(*args):
-    for var, new_value in zip(lol.index,args):
-        sim.model.mvars.current.loc[var, 'Value'] = new_value
-    return None
+    try:
+        new_pick = model_names.index(button_id)
+    except:
+        new_pick = 0
+        
+    global mysim, myvars
+    mysim, myvars = sim(model_names[new_pick])
+    return dropdown_models(new_pick), mvars(myvars)
 
+
+# callback to update the model variables with the sliders / input box
+@app.callback(
+    [Output('slider-'+var+'-input', 'value') for var in myvars.index],
+    [Output('slider-'+var, 'value') for var in myvars.index],
+    [Input('slider-'+var+'-input', 'value') for var in myvars.index],
+    [Input('slider-'+var, 'value') for var in myvars.index])
+
+def update_mvars_slider(*args):
+    sliders = list(args[int(len(args)/2):])
+    inputs = list(args[:int(len(args)/2)])
+
+    ctx = dash.callback_context
+    button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    for i,var in enumerate(myvars.index):
+        if 'input' in button_id:
+            sliders[i] = inputs[i]
+        else:
+            inputs[i] = sliders[i]
+        mysim.model.mvars.current.loc[var, 'Value'] = sliders[i]
+
+    return (*inputs, *sliders)
+
+# callback to run the simulator and display data when the button is clicked
 @app.callback(
     [
-        Output('sim_plot', 'figure'),
-        Output('mvars','children')
+        Output('sim_plot4', 'figure'),
+        Output('mvars4','children')
     ],
     Input('btn_run', 'n_clicks'))
 def update_figure(n_clicks):
-    data = sim.run()
-    table = generate_table(sim.model.mvars.current)
+    data = mysim.run()
+    table = generate_table(mysim.model.mvars.current)
     fig = px.scatter(data)
-    sim.model.reset()
+    mysim.model.reset()
     return fig, table
-
-# this callback uses the current pathname to set the active state of the
-# corresponding nav link to true, allowing users to tell see page they are on
-# @app.callback(
-#     [Output(f"page-{i}-link", "active") for i in range(1, 4)],
-#     [Input("url", "pathname")],
-# )
-# def toggle_active_links(pathname):
-#     if pathname == "/":
-#         # Treat page 1 as the homepage / index
-#         return True, False, False
-#     return [pathname == f"/page-{i}" for i in range(1, 4)]
-
-
-# @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
-# def render_page_content(pathname):
-#     if pathname in ["/", "/page-1"]:
-#         return html.P("This is the content of page 1!")
-#     elif pathname == "/page-2":
-#         return html.P("This is the content of page 2. Yay!")
-#     elif pathname == "/page-3":
-#         return html.P("Oh cool, this is page 3!")
-#     # If the user tries to reach a different page, return a 404 message
-#     return dbc.Jumbotron(
-#         [
-#             html.H1("404: Not found", className="text-danger"),
-#             html.Hr(),
-#             html.P(f"The pathname {pathname} was not recognised..."),
-#         ]
+'''
