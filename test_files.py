a67fd652182213c2e6c76308996f9070bf95dcae
Shweta Bhushan <sb21321@gmail.com>
2021-03-15 15:00:32 -0700

    first unit test

---
+++ test_files.py
@@ -0,0 +1,12 @@
+import unittest
+import rms
+from rms import main2
+
+class Testfiles(unittest.TestCase):
+
+    def test_path(display_page):
+        isExist = os.path.exists(pathname)
+        assertTrue(isExist)
+    def test_file(display_page):
+        isFile = os.path.isfile(pathname)
+        assertTrue(isFile)
