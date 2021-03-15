5dd47500aaa0c0a3258398484678c6ad7aa9b6c1
Shweta Bhushan <sb21321@gmail.com>
2021-03-15 15:52:11 -0700

    modified unit tests

--- test_files.py
+++ test_files.py
@@ -1,12 +1,15 @@
 import unittest
 import rms
 from rms import main2
+import os
 
 class Testfiles(unittest.TestCase):
 
-    def test_path(display_page):
-        isExist = os.path.exists(pathname)
-        assertTrue(isExist)
-    def test_file(display_page):
-        isFile = os.path.isfile(pathname)
-        assertTrue(isFile)
+    def test_display_page1(self):
+        tester_path = '/'
+        isExist = os.path.exists(main2.display_page(tester_path)
+        self.assertTrue(isExist)
+    def test_display_page2(self):
+        tester_path = '/'
+        isFile = os.path.isfile(main2.display_page(tester_path))
+        self.assertTrue(isFile)
